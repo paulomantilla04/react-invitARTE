dee2420ee1369135aa5464ec8e9414fa44e2fc6a
unknown <ma475671@uaeh.edu.mx>
2024-04-08 08:41:54 -0600

    instagram added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
     <footer className='flex items-center justify-center h-14 bg-[#2B2B2B] text-white max-[768px]:flex-col'>
         <div className='flex items-center justify-center gap-5'>
             <p className='font-roboto font-light text-xs lg:text-sm'>©2024 Invitarte. Todos los derechos reservados</p>
-            <a href='https://www.instagram.com/' target='blank' className='hover:scale-90 transiton-all duration-300'>
+            <a href='https://www.instagram.com/invitarte._' target='blank' className='hover:scale-90 transiton-all duration-300'>
                 <Instagram/>
             </a>
         </div>

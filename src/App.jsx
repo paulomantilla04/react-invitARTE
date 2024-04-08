6b7e7a51afc7dae3edc03af9cf863af552cb48f2
unknown <ma475671@uaeh.edu.mx>
2024-04-07 18:05:48 -0600

    index modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header"
 import Page from "./pages/Page";
-import Footer from "./components/Footer";
 
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
